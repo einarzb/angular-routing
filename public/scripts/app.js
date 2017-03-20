8327ab7ea94daf9a90f16eec1a791ffdb501d39b
einarzb <einarzb@gmail.com>
2017-03-20 09:06:52 +0200

    added home state and some basic rules to follow in sap

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,14 @@
 var app = angular.module('scotcha', ['ui.router']);
+//injecting ui.router
 
 app.config(function($stateProvider, $urlRouterProvider) {
+  //homepage
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: 'partial-home.html'
+    //vanilla way: template:'<h1>hello world!</h1>'
+  });
 
 }); 
+
+
