48838e6757d1452ad9388fb879f13da6e10611f1
einarzb <einarzb@gmail.com>
2017-03-21 00:20:44 +0200

    added nested state in home.list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,20 +2,23 @@ var app = angular.module('scotcha', ['ui.router']);
 //injecting ui.router
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise('/home'); //re-route the user to the URL that activates our home state if it otherwise can't find a matching route.
 
-  //homepage
-  $stateProvider.state('home', {
+//homepage
+  $stateProvider
+    .state('home', {
     url: '/home',
     templateUrl: 'partial-home.html'
     //vanilla way: template:'<h1>hello world!</h1>'
-  });
+  })
+    .state('home.list', {
+    url:'/list',
+    templateUrl: 'partial-home-list.html'
+  })
 
  $stateProvider.state('contact', {
   url: '/contact',
   templateUrl: 'partial-contact.html'
   });
 
-}); 
-
-
+});
