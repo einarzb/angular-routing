a6feb824690243e9c3bdbcdd4733c9847a9bac9c
einarzb <einarzb@gmail.com>
2017-03-20 23:22:21 +0200

    added partial contact

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,18 @@ var app = angular.module('scotcha', ['ui.router']);
 //injecting ui.router
 
 app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
   //homepage
   $stateProvider.state('home', {
     url: '/home',
     templateUrl: 'partial-home.html'
     //vanilla way: template:'<h1>hello world!</h1>'
+  });
+
+ $stateProvider.state('contact', {
+  url: '/contact',
+  templateUrl: 'partial-contact.html'
   });
 
 }); 
