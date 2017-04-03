4389ec454d7abbbe58a1bb70e79f0a4b154274e9
einarzb <einarzb@gmail.com>
2017-04-03 10:22:56 +0300

    changed styling and html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,28 @@
-var app = angular.module('igg', ['ui.router']);
-//injecting ui.router
+var app = angular.module('igg', ['ui.router']); //injecting ui.router
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/home/review'); //re-route the user to the URL that activates our home state if it otherwise can't find a matching route.
+  //re-route the user to the URL that activates our home state if it otherwise can't find a matching route.
+  $urlRouterProvider.otherwise('/home');
 
 //homepage
   $stateProvider
     .state('home', {
     url: '/home',
-    templateUrl: 'partial-home.html'
+    templateUrl: '../templates/partial-home.html'
     //vanilla way: template:'<h1>hello world!</h1>'
   }) //nested state
     .state('home.list', {
     url:'/list',
-    templateUrl: 'partial-home-list.html'
+    templateUrl: '../templates/partial-home-list.html'
   }) //nested reviews
     .state('home.review', {
     url:'/review',
-    templateUrl:'partial-home-review.html'
+    templateUrl:'../templates/partial-home-review.html'
   })
 
  $stateProvider.state('contact', {
   url: '/contact',
-  templateUrl: 'partial-contact.html'
+  templateUrl: '../templates/partial-contact.html'
   });
 
 });
